a782d3b4db50f2b6242ad3d094c9937669f44faf
Kunal Arora <10kunalji@gmail.com>
2021-06-13 15:20:58 +0530

    almost done

---
+++ src/context/Context.js
@@ -0,0 +1,3 @@
+//TODO: Done Create context: ContactContext
+import { createContext } from "react"
+export const ContactContext = createContext()
---
+++ src/utils/config.js
@@ -0,0 +1,20 @@
+//TODO: add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+ export  const firebaseConfig = {
+   apiKey: "AIzaSyDHLFIgy2KpAH89hpYFhQq36GSdym4gn40",
+   authDomain: "github-finder-9f607.firebaseapp.com",
+   databaseURL:
+     "https://github-finder-9f607-default-rtdb.asia-southeast1.firebasedatabase.app",
+   projectId: "github-finder-9f607",
+   storageBucket: "github-finder-9f607.appspot.com",
+   messagingSenderId: "790528034079",
+   appId: "1:790528034079:web:08aeb3e57acfd8a0789c2a",
+   measurementId: "G-458M15CSV2",
+ };
